80aabc3bf6739b4c80129050bf515ee2dc1e94d0
cedric angelo plasabas <cedricangelo.plasabas@gmail.com>
2019-08-07 22:25:00 +0800

    build

---
+++ dist/precache-manifest.50c380e96bcfc0a74a1f355193e2e136.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9c3d92418cfe630cb68",
+    "url": "/js/chunk-c980db7e.916f8d6a.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "edfbbfee53ac16621461",
+    "url": "/js/chunk-cd7a9e6e.9a2ec519.js"
+  },
+  {
+    "revision": "bbd9218867772c7d4b59",
+    "url": "/js/chunk-2112c069.a9298e1f.js"
+  },
+  {
+    "revision": "1ecaa6f71597cd4b084c",
+    "url": "/js/chunk-7f8d3ef0.5434b4e9.js"
+  },
+  {
+    "revision": "6ff6be1e7f6bc1f1c7a9",
+    "url": "/js/chunk-vendors.911737d7.js"
+  },
+  {
+    "revision": "fe78019040ce1f9faa18",
+    "url": "/js/chunk-8fe7d95c.9ffc01d9.js"
+  },
+  {
+    "revision": "d9e9fdcbf34c759a0840",
+    "url": "/js/chunk-2d0ae90a.615e57af.js"
+  },
+  {
+    "revision": "d23650b6f2016d358ccb",
+    "url": "/js/app.8b6c1f81.js"
+  },
+  {
+    "revision": "9fb172ac284db644c461225c7690495f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "936e5288add5b0b27349e9ff4fa8a1d6",
+    "url": "/img/rider-dash-logo-white.936e5288.png"
+  },
+  {
+    "revision": "d02bab25d44a8803554f94dbbd6c4a1d",
+    "url": "/img/rider-dash-logo-text.d02bab25.png"
+  },
+  {
+    "revision": "6ff6be1e7f6bc1f1c7a9",
+    "url": "/css/chunk-vendors.abff8dda.css"
+  },
+  {
+    "revision": "e9c3d92418cfe630cb68",
+    "url": "/css/chunk-c980db7e.71e07f44.css"
+  },
+  {
+    "revision": "fe78019040ce1f9faa18",
+    "url": "/css/chunk-8fe7d95c.75ad2578.css"
+  },
+  {
+    "revision": "d23650b6f2016d358ccb",
+    "url": "/css/app.c1b91487.css"
+  }
+];
